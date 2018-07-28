66c348debe15d71f0296548a370146acd380e610
Jae Yong Kim <kim900180@gmail.com>
2018-07-28 01:11:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,3 +20,16 @@ def assign_rooms(array)
   end
   return batch
 end
+
+def printer
+  counter_message = 0
+  counter_room = 0
+  while counter_message < batch_badge_creator(array).size
+    puts batch_badge_creator[counter_message]
+    counter_message += 1
+  end
+  while counter_room < assign_rooms(array).size
+    puts assign_rooms[counter_room]
+    counter_room += 1
+  end
+end
